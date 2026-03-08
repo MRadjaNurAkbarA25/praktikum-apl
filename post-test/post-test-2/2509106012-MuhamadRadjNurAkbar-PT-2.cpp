1e31526e8e8be18ca354632d6e1e253b777db408
MRadjaNurAkbarA25 <gulalimerahjambu16@gmail.com>
2026-03-08 11:43:32 +0700

    Selesai Bagian Login/Regis

--- post-test/post-test-2/2509106012-MuhamadRadjNurAkbar-PT-2.cpp
+++ post-test/post-test-2/2509106012-MuhamadRadjNurAkbar-PT-2.cpp
@@ -10,7 +10,7 @@ struct User {
 };
 
 int main() {
-    User users[5];
+    User users[100];
     int jumlahUser = 2;
     int pilihan;
     users[0] = {"r", "012", "admin"};
@@ -39,20 +39,78 @@ int main() {
 
                 while (kesempatan > 0 && !loginSukses) {
                     cout << "Silahkan login untuk melanjutkan\n";
-                    cout << "Username: ", cin >> username;
-                    cout << "Password: ", cin >> password;
+                    cout << "Username: "; 
+                    cin >> username;
+                    cout << "Password: "; 
+                    cin >> password;
+                    bool ditemukan = false;
 
                     for (int i = 0; i < jumlahUser; i++) {
                         if (username == users[i].usr && password == users[i].pw ) {
                             loginSukses = true;
                             roleUser = users[i].role;
+                            ditemukan = true;
                             cout << "\nLogin berhasil! Selamat datang, " << username << "!\n\n";
                             break;
                         }
                     }
+                    if (!ditemukan) {
+                        kesempatan--;
+                        cout << "\nUsername dan password salah!\n";
+                        cout << "Sisa kesempatan login: " << kesempatan << "\n"; 
+                    }
                     
                 }
+                if (!loginSukses) {
+                    cout << "Kesempatan login telah habis, coba lagi nanti!\n";
+                    return 0;
+                }
+                break;
             } // case 1
+            case 2 : {
+                if (jumlahUser >= 100) {
+                    cout << "Database penuh! tidak bisa register";
+                    break;
+                }
+                string usernameBaru, passwordBaru;
+                cout << "Masukkan username baru: ";
+                cin.ignore();
+                getline(cin, usernameBaru);
+                if (usernameBaru == "") {
+                    cout << "Username tidak boleh kosong!\n";
+                    break;
+                }
+                bool available = false;
+                for (int i = 0; i < jumlahUser; i++) {
+                    if (usernameBaru == users[i].usr) {
+                        available = true;
+                        break;
+                    }
+                }
+                if (available) {
+                    cout << "Username sudah digunakan!\n";
+                    break;
+                }
+                cout << "Masukkan password baru: ";
+                getline(cin, passwordBaru);
+                if (passwordBaru.length() < 3) {
+                    cout << "Panjang password minimal 3 karakter!\n";
+                    break;
+                }
+                users[jumlahUser] = {usernameBaru, passwordBaru, "user"};
+                jumlahUser++;
+                cout << "Registrasi berhasil!\n";
+                break;
+            } // case 2
+            case 3 : {
+                cout << "Kamu menekan tombol keluar!";
+                return 0;
+            }
+            default : {
+                cout << "Pilihan tidak valid!";
+                break;
+            }
+
         } // switch 1
 
     } // while menu 1
